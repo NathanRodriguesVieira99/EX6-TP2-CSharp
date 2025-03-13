1fadbbbf9db1a5f35e2b76dc42d587c6cf5ff792
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-03-13 18:54:43 -0300

    feat: exercise 6 done (w a error that a don´t understand)

---
+++ Ex6-TP2-CSharp/Ex6.cs
@@ -0,0 +1,31 @@
+﻿class Ex6
+{
+    static void Main()
+    {
+        // dessa forma le o que esta no input
+        Console.WriteLine("Informe seu peso: ");
+        string pesoInformado = Console.ReadLine();
+
+        // aqui valido se o peso é valido
+        if (!double.TryParse(pesoInformado, out double peso) || peso <= 0)
+        {
+            Console.WriteLine("Peso inválido");
+            return;
+        }
+
+        // dessa forma le o que esta no input
+        Console.WriteLine("Informe sua altura ");
+        string alturaInformada = Console.ReadLine();
+
+        // aqui valido se altura é valida
+        if (!double.TryParse(alturaInformada, out double altura) || altura <= 0)
+        {
+            Console.WriteLine("Altura inválida");
+            return;
+        }
+
+        // calculo do IMC
+        double IMC = peso / (altura * altura);
+        Console.WriteLine($"Seu IMC: {IMC:F2}");
+    }
+}
