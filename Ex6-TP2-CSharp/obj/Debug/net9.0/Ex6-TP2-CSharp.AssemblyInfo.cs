474b6d055009d5b273c8fddf3be0c507e332d2c0
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-03-14 07:28:23 -0300

    feat: ex 6 done

--- Ex6-TP2-CSharp/obj/Debug/net9.0/Ex6-TP2-CSharp.AssemblyInfo.cs
+++ Ex6-TP2-CSharp/obj/Debug/net9.0/Ex6-TP2-CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ex6-TP2-CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fadbbbf9db1a5f35e2b76dc42d587c6cf5ff792")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36f3121080bd39c16cff46df91cbea12f6820b7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ex6-TP2-CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ex6-TP2-CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
